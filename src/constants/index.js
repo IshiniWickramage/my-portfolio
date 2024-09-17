bba7353bf0061094b04af8bc7a0d580d9ec0a6a7
IshiniWickramage <147267658+IshiniWickramage@users.noreply.github.com>
2024-09-17 20:57:03 +0530

    Added all my information

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,7 +3,7 @@ import project2 from "../assets/projects/project-2.webp";
 import project3 from "../assets/projects/project-3.webp";
 import project4 from "../assets/projects/project-4.webp";
 
-export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications.I have honed my skills in front-end technologies like React , as well as back-end technologies like .NetCore , MySQL and MSSQL. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+export const HERO_CONTENT = `I'm a full stack developer with a passion for building dynamic and responsive web applications. I work with front-end and back-end technologies like React.js,.NET, MySQL, MSSQL.`;
 
 export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React,.NetCore, MySQL and MSSQL. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
 
@@ -26,7 +26,7 @@ export const PROJECTS = [
     title: "Reservation Management System",
     image: project1,
     description:
-      "The web-based reservation management system is tailored for large organizations with multiple companies, enabling staff to manage reservations made by phone or in-person. This system simplifies the reservation process for customers and enhances organizational control, improving efficiency and reducing complexity in reservation management.",
+      "The web-based reservation management system is a group project that tailored for large organizations with multiple companies, enabling staff to manage reservations made by phone or in-person. This system simplifies the reservation process for customers and enhances organizational control, improving efficiency and reducing complexity in reservation management.",
     technologies: ["React","Scss", ".NetCore", "MSSQL"],
   },
   {
@@ -47,12 +47,12 @@ export const PROJECTS = [
     title: "Message Sharing System",
     image: project4,
     description:
-      "  This is a system that allows users to send and receive messages to and from other users. These systems can take various forms, including email platforms, chat applications, social media platforms, and even SMS messages",
+      "  This is a group project which is a system that allows users to send and receive messages to and from other users. These systems can take various forms, including email platforms, chat applications, social media platforms, and even SMS messages",
     technologies: ["HTML", "CSS", "JavaScript", "PHP", "MySQL"],
   },
 ];
 
 export const CONTACT = {
-  address: "Weeraketiya, Hambanthota ",
+  address: "Hambanthota District, Sri Lanka ",
   email: "ishiniwickramage@gmail.com",
 };
