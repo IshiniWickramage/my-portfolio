9c53d48459284bbe15f421f48ebe49ef4f4d9e68
IshiniWickramage <147267658+IshiniWickramage@users.noreply.github.com>
2024-09-17 12:38:07 +0530

    Initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,37 @@
+import { FaFacebook, FaGithub, FaLinkedin } from "react-icons/fa"
+import logo from "../assets/ishiniLogo.webp"
+//https://github.com/IshiniWickramage/my-portfolio.git
+
+const Navbar = () => {
+  return (
+    <nav className="flex items-center justify-between py-6">
+      <div className="flex flex-shrink-0 items-center">
+        <a href="/" aria-label="Home">
+        <img src={logo} className="mx-2" width={50} height={33} alt="Logo"/>
+        </a>
+      </div>
+      <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+        <a href="https://www.linkedin.com/in/ishini-wickramage-0b6733264" 
+        target="_blank"
+        rel="noopener noreferrer"
+        aria-label="LinkedIn">
+        <FaLinkedin/>
+        </a>
+        <a href="https://github.com/IshiniWickramage" 
+        target="_blank"
+        rel="noopener noreferrer"
+        aria-label="GitHub">
+        <FaGithub/>
+        </a>
+        <a href="https://www.facebook.com/profile.php?id=100071967105431&mibextid=ZbWKwL" 
+        target="_blank"
+        rel="noopener noreferrer"
+        aria-label="Facebook">
+        <FaFacebook/>
+        </a>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
